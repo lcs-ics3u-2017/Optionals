a1f508218aa8d0ead9e0aac8d498c3ab3d8c4d5e
Russell Gordon <github@russellgordon.ca>
2019-10-04 13:37:05 -0400

    Remove bit about implicit conversion

--- Optionals/Exploring Optionals.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
+++ Optionals/Exploring Optionals.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -43,16 +43,10 @@
 /*:
  
  Run the playground again.
- 
- You will notice a warning appears:
- 
- ![coerced](coerced.png)
- 
- Aside from that warning, focus on the result shown in the debug area.
- 
+  
  How does the result shown from the second block of code differ from the result you got when running the first block of code?
  
- Write a response to this question in your app development journal.
+ Write a response to this question in your black notebook.
  
  */
 //: [Next](@next)
